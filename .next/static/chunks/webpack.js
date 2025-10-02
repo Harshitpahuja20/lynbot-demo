05918c33eef41f0ca12bd4bc01a9473b1e352181
fmwctodev <srichard358@gmail.com>
2025-10-02 02:25:37 -0400

    Create .env.local with Supabase variables

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b266da40d4d246d6"; }
+/******/ 		__webpack_require__.h = function() { return "7f6350f6028f40c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.5717855c04a0bd40.hot-update.js
+++ .next/static/webpack/webpack.5717855c04a0bd40.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b266da40d4d246d6"; }
+/******/ 	__webpack_require__.h = function() { return "7f6350f6028f40c8"; }
 /******/ }();
 /******/ 
 /******/ }
