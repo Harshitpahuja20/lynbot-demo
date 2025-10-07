e1349111a29698c63cf0a83fa0d4aa926d2b9e0f
fmwctodev <srichard358@gmail.com>
2025-10-06 23:32:02 -0400

    Fix database setup and user registration

--- .next/static/webpack/webpack.e65724835a3537f5.hot-update.js
+++ .next/static/webpack/webpack.e65724835a3537f5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c628997dc7c7575b"; }
+/******/ 	__webpack_require__.h = function() { return "580331696d10a642"; }
 /******/ }();
 /******/ 
 /******/ }
